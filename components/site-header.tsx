8eae7aed7082a7cce775300d6968b9d5dd6efc35
NamAnhTran99 <NamAnhTran99@users.noreply.github.com>
2026-08-27 21:41:57 +1000

    Implement Radix UI to navigation menu

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import Link from "next/link";
+import { NavigationMenu } from "radix-ui";
 
 export function SiteHeader() {
   return (
@@ -8,18 +11,38 @@ export function SiteHeader() {
         <span>ROVER</span>
       </Link>
 
-      <nav className="site-nav" aria-label="Primary navigation">
-        <div className="nav-dropdown">
-          <Link className="nav-dropdown-trigger" href="/#rovers">Rovers</Link>
-          <div className="nav-dropdown-menu">
-            <Link href="/rovers/borealis">Borealis</Link>
-            <Link href="/rovers/aurora">AURORA</Link>
-            <span className="nav-dropdown-pending" aria-disabled="true">Chasmata</span>
-          </div>
-        </div>
-        <Link href="/#mission">Mission</Link>
-        <Link href="/#support">Support</Link>
-      </nav>
+      <NavigationMenu.Root className="site-nav" aria-label="Primary navigation">
+        <NavigationMenu.List className="site-nav-list">
+          <NavigationMenu.Item>
+            <NavigationMenu.Trigger className="nav-dropdown-trigger">Rovers</NavigationMenu.Trigger>
+            <NavigationMenu.Content className="nav-dropdown-menu">
+              <ul>
+                <li>
+                  <NavigationMenu.Link asChild>
+                    <Link href="/rovers/borealis">Borealis</Link>
+                  </NavigationMenu.Link>
+                </li>
+                <li>
+                  <NavigationMenu.Link asChild>
+                    <Link href="/rovers/aurora">AURORA</Link>
+                  </NavigationMenu.Link>
+                </li>
+                <li><span className="nav-dropdown-pending" aria-disabled="true">Chasmata</span></li>
+              </ul>
+            </NavigationMenu.Content>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link asChild>
+              <Link href="/#mission">Mission</Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+          <NavigationMenu.Item>
+            <NavigationMenu.Link asChild>
+              <Link href="/#support">Support</Link>
+            </NavigationMenu.Link>
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+      </NavigationMenu.Root>
     </header>
   );
 }
